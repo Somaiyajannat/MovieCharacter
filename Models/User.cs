fb3ba845f75eea0b6807a965fde131cfe3fa1776
Somaiya Jannat <somaiyajannat044@gmail.com>
2023-08-17 16:34:55 +0600

    modified model

--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,11 @@ namespace MovieCharacter.Models;
 
 public class User{
     public int Id{get;set;}
-    public string Username{get;set;}
+    public string? Username{get;set;}
     public byte[] PasswordHash{get;set;} = new byte[0];
     public byte[] PasswordSalt{get;set;} = new byte[0];
+    // relation purpose
+    public List<Character>? Characters{get;set;}
+
 
 }
