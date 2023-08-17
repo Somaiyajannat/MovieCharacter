0eb1e8196ec9249495dda3048bf0c176a05465a7
Somaiya Jannat <somaiyajannat044@gmail.com>
2023-08-17 17:37:36 +0600

    modifies login

--- DTO/User/UserLoginDto.cs
+++ DTO/User/UserLoginDto.cs
@@ -2,5 +2,5 @@ namespace MovieCharacter.DTO.User;
 
 public class UserLoginDto{
     public string Username { get; set; } = string.Empty;
-    public string Password{get;set;} = string.Empty;
+    public string Password {get; set;} = string.Empty;
 }
